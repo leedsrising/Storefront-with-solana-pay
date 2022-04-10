6fc1cd0cb1701e7a2df5f2799c560ae412924554
leedsrising <leedsrising@gmail.com>
2022-04-10 16:09:59 -0400

    :sparkles: core payment flow, allow payment in USDC

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('5Ma2xY94q5NB7GqGD1mRLGLARQpYfX4uSEEoFbUbmeUV')
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
